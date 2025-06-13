d889691d1b01307aeaf70d703e17a5ebb62d5ce0
Braxton Bailey <b.e.bailey81@gmail.com>
2025-06-13 00:14:55 -0400

    bugg fii xx bug bug fix

--- types.d.ts
+++ types.d.ts
@@ -1,9 +1,9 @@
-// types.d.ts
-export {};
+/// <reference types="@cloudflare/workers-types" />
 
-declare global {
-  interface CloudflareEnv {
-    JIMI_DB: D1Database;
-  }
+declare namespace CloudflareEnv {
+  /** your D1 database binding */
+  export const JIMI_DB: D1Database;
+  /** your R2 bucket binding */
+  export const PRISIM_BUCKET: R2Bucket;
 }
 
