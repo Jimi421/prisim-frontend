6653652fde411346b41c24255ef4cac786365fe6
Braxton Bailey <b.e.bailey81@gmail.com>
2025-07-05 12:07:41 -0400

    fix wrapper config

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,4 +1,8 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
+
+// Use the workers wrapper which is one of the valid options in
+// OpenNext 3.1.3. Other supported wrappers include aws-lambda,
+// aws-lambda-edge, cloudflare, cloudflare-pages and netlify.
 export default defineCloudflareConfig({
-  wrapper: "cloudflare-pages",
+  wrapper: "cloudflare-workers",
 });
