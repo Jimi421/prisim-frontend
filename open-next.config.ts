3a382be5ac6eca8c89fab6df7a3e938c57f90e53
Braxton Bailey <b.e.bailey81@gmail.com>
2025-07-05 11:54:54 -0400

    bug fix

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,8 +1,4 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
-import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
-
 export default defineCloudflareConfig({
-  wrapper: "cloudflare-pages",              // This targets Cloudflare Pages (not Workers)
-  incrementalCache: r2IncrementalCache,     // This enables R2 for incremental caching
+  wrapper: "cloudflare-pages",
 });
-
